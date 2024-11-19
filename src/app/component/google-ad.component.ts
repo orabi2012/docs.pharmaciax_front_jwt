89e204fc5a9df893cfd7edf12bda25e1163a1df7
Ahmed Orabi <aorabi@outlook.com>
2024-11-19 08:49:03 +0300

    google ads responsive

--- src/app/component/google-ad.component.ts
+++ src/app/component/google-ad.component.ts
@@ -3,13 +3,26 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
     selector: 'app-google-ad',
     template: `
-    <ins class="adsbygoogle"
-         style="display:block"
-         [attr.data-ad-client]="adClient"
-         [attr.data-ad-slot]="adSlot"
-         [attr.data-ad-format]="adFormat"
-         data-full-width-responsive="true"></ins>
-  `
+    <div class="ad-container">
+        <ins class="adsbygoogle"
+             style="display:block; width:100%;"
+             [attr.data-ad-client]="adClient"
+             [attr.data-ad-slot]="adSlot"
+             [attr.data-ad-format]="adFormat"
+             data-full-width-responsive="true"></ins>
+    </div>
+    `,
+    styles: [`
+        .ad-container {
+            width: 100%;
+            max-width: 100%;
+            overflow: hidden;
+            margin: 0 auto;
+        }
+        ins.adsbygoogle {
+            max-width: 100%;
+        }
+    `]
 })
 export class GoogleAdComponent implements OnInit {
     @Input() adClient: string = 'ca-pub-8400668080120923';
