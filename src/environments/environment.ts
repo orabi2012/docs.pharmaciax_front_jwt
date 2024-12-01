ad2cca7b2f4633ee9a192ad40237810fb649ba0e
Ahmed Orabi <aorabi@outlook.com>
2024-12-01 22:59:30 +0300

    backend = vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   // base_url: 'https://wild-tan-elk-robe.cyclic.app/',
-   base_url: 'https://pharmaciax-api.onrender.com/',
-  //base_url: 'http://localhost:3000/',
+  // base_url: 'https://pharmaciax-api.onrender.com/',
+  base_url: 'http://localhost:3000/',
 
   inactive: 'File-status-List',
 };
