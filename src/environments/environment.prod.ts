02106ba145ad4d94ec213ba1aca88d85025be636
Ahmed Orabi <aorabi@outlook.com>
2024-12-07 23:34:17 +0300

    on render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  base_url: 'https://pharmaciax-node-js-mysql.vercel.app/',
+  // base_url: 'https://pharmaciax-node-js-mysql.vercel.app/',
   // base_url: 'https://localhost:3000/',
-  // base_url: 'https://pharmaciax-api.onrender.com/',
+  base_url: 'https://pharmaciax-api.onrender.com/',
 
   inactive: 'File-status-List',
 };
